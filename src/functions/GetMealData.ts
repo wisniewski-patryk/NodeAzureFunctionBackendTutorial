5d2972476d2158e4ab247b603ec3ac91f524afc3
Patryk Wiśniewski <46683778+wisniewski-patryk@users.noreply.github.com>
2024-12-24 09:05:43 +0000

    download blob function

--- src/functions/GetMealData.ts
+++ src/functions/GetMealData.ts
@@ -1,12 +1,52 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { BlobServiceClient } from "@azure/storage-blob";
+
+
+const sasToken = "https://<my-blob-address>.blob.core.windows.net.com?<my-sas-token>";
+const blobServiceClient = new BlobServiceClient(sasToken);
+
 
 export async function GetMealData(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const { containerName, blobName } = await request.json();
 
-    return { body: `Hello, ${name}!` };
+    if (!containerName || !blobName){
+        return { status: 400, body: "getting container or blob name fail" };
+    }
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const blobClient = containerClient.getBlobClient(blobName);
+        const downloadBlockBlobResponse = await blobClient.download();
+
+        const downloaded = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
+
+        return {
+            status: 200,
+            body: downloaded,
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Content-Disposition': `attachment; filename="${blobName}"`
+            }
+        };
+    } catch (error) {
+        context.error(`Error downloading blob: ${error.message}`);
+        return { status: 500, body: "Error downloading blob" };
+    }
 };
+
+async function streamToBuffer(readableStream: NodeJS.ReadableStream): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const chunks: Buffer[] = [];
+        readableStream.on("data", (data) => {
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on("end", () => {
+            resolve(Buffer.concat(chunks));
+        });
+        readableStream.on("error", reject);
+    });
+}
 
 app.http('GetMealData', {
     methods: ['GET'],
